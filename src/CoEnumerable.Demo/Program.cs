1af928c68aa462f91f68ae335047f40d3da2afed
Luis O'Shea <loshea@gmail.com>
2020-05-22 17:12:01 -0400

    Initial commit

---
+++ src/CoEnumerable.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoEnumerable.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
