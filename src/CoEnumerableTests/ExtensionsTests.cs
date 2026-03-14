4a02421b58703e214dd5e0b58b553142ce2d7157
Luis O'Shea <loshea@gmail.com>
2026-03-14 12:46:48 -0400

    Tidy up resharper warnings

--- src/CoEnumerableTests/ExtensionsTests.cs
+++ src/CoEnumerableTests/ExtensionsTests.cs
@@ -1,54 +1,72 @@
-﻿using CoEnumerableTests;
+﻿using System.Collections.Generic;
+using CoEnumerableTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+// xxReSharper disable ConvertToLocalFunction
 
 namespace CoEnumerable.Tests
 {
     [TestClass()]
     public class ExtensionsTests
     {
+        private static bool Coenumerable1(IEnumerable<int> ns) => ns.Any(n => n == 3);
+        private static int Coenumerable2(IEnumerable<int> ns) => ns.Take(2).Sum();
+        
+        private static readonly IEnumerable<int> Nums = Enumerable.Range(1, 2_000_000);
+        
         [TestMethod()]
         public void CombineTest()
         {
-            var nums = Enumerable.Range(1, 2_000_000);
-            (var x, var y) = nums.Combine(ns => ns.Any(n => n == 3), ns => ns.Take(2).Sum(), (x, y) => (x, y));
-            Assert.IsTrue(x);
-            Assert.AreEqual(3, y);
+            // ReSharper disable PossibleMultipleEnumeration
+            var (x1, x2) = Nums.Combine(Coenumerable1, Coenumerable2, (x, y) => (x, y));
+            Assert.AreEqual(Coenumerable1(Nums), x1);
+            Assert.AreEqual(Coenumerable2(Nums), x2);
+            // ReSharper restore PossibleMultipleEnumeration
         }
 
         [TestMethod()]
         public void CombineTracingTest()
         {
-            var nums = Enumerable.Range(1, 2_000_000);
-            (var x, var y) = nums.Tracing(out var sb).Combine(
-                ns => ns.Any(n => n == 3),
-                ns => ns.Take(2).Sum(),
+            // ReSharper disable PossibleMultipleEnumeration
+            var (x1, x2) = Nums.Tracing(out var sb).Combine(Coenumerable1, Coenumerable2,
                 (x, y) => (x, y),
-                out int tid1,
-                out int tid2);
-            Assert.IsTrue(x);
-            Assert.AreEqual(3, y);
+                out var tid1,
+                out var tid2);
+            Assert.AreEqual(Coenumerable1(Nums), x1);
+            Assert.AreEqual(Coenumerable2(Nums), x2);
 
             // The problem with tracking the precise activities triggered by Combine(...) is that the threads
-            // associated with the two coenumerables may various orders.  Below we painfully create a regex
+            // associated with the two coenumerables may run in various orders.  Below we painfully create a regex
             // to allow for this variation.  We define some local functions to make it a bit easier.
             var pat = new StringBuilder(@$"^{Task.CurrentId}:, ");
-            // below we define functions that add to the pattern corresponding to various events
-            void a<S, T>(S s, T t) => pat.Append($"{s}:{t} "); // data
-            void mn(bool b) => a($"({tid1}|{tid2})", b); // MoveNext() by some thread
-            void c(int i) { pat.Append("("); a(tid1, i); a(tid2, i); pat.Append("|"); a(tid2, i); a(tid1, i); pat.Append(")"); }  // Current by both threads
-            void c1(int i) => a(tid1, i); // Current by just thread 1
-            using (var enumerator = nums.GetEnumerator())
-                for (int i = 0; i < 3; i++)
+            using (var enumerator = Nums.GetEnumerator())
+                for (var i = 0; i < 3; i++)
                 {
-                    mn(enumerator.MoveNext());
-                    if (i < 2) c(enumerator.Current); else c1(enumerator.Current);
+                    Mn(enumerator.MoveNext());
+                    if (i < 2) C(enumerator.Current); else C1(enumerator.Current);
                 }
             pat.Append($"{Task.CurrentId}:; $");
             Assert.IsTrue(Regex.IsMatch(sb.ToString(), pat.ToString()));
+            // ReSharper restore PossibleMultipleEnumeration
+            return;
+            
+            // below we define functions that add to the pattern corresponding to various events
+            void A<TS, T>(TS s, T t) => pat.Append($"{s}:{t} "); // data
+            void Mn(bool b) => A($"({tid1}|{tid2})", b); // MoveNext() by some thread
+            void C(int i)
+            {
+                pat.Append('(');
+                A(tid1, i); 
+                A(tid2, i); 
+                pat.Append('|'); 
+                A(tid2, i);
+                A(tid1, i); 
+                pat.Append(')');
+            }  // Current by both threads
+            void C1(int i) => A(tid1, i); // Current by just thread 1
         }
     }
 }
