3a10679d07356eb1e6566849044d0111ec3701f1
Luis O'Shea <loshea@gmail.com>
2026-03-14 12:47:26 -0400

    Remove comment

--- src/CoEnumerableTests/ExtensionsTests.cs
+++ src/CoEnumerableTests/ExtensionsTests.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-// xxReSharper disable ConvertToLocalFunction
 
 namespace CoEnumerable.Tests
 {
